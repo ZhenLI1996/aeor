c443a4c29c6105c136e28b36023aca6943a88f4f
luciferlee1996 <luciferlee1996@hotmail.com>
2017-12-09 11:51:26 +0800

    oxford divide

--- oxford_test/extract.py
+++ oxford_test/extract.py
@@ -6,6 +6,7 @@ import os
 import re
 import json
 import time
+import pandas as pd
 
 DEFAULT_RULE = r'<meta property="og:title" content="([^/<>]*?)" />'
 
@@ -84,58 +85,66 @@ def find_derivative_of_or_origin(l, oxford_dir="oxford",
 
 
 
+
+
 def __divide_single_word(word,
-                         oxford_dir="oxford",
-                         deriv_rule=r'<p class="derivative_of">See <a href="(.*?)">\1</a></p>',
-                         origin_rule=r'<section class="etymology etym"><h3><strong>Origin</strong></h3><div class="senseInnerWrapper"><p>(.*?)</p></div></section>',
-                         result_rule=r'<meta property="og:title" content="([^/<>]*?)" />'):
+                         deriv_r, origin_r, result_r,
+                         oxford_dir="oxford"):
   fpath = os.path.join(oxford_dir, word + ".html")
-  deriv_r = re.compile(deriv_rule)
-  origin_r = re.compile(origin_rule)
-  result_r = re.compile(result_rule)
   if not os.path.exists(fpath):
-    return "file not exists!"
-
-
+    return "file not exists!", -9
   with open(fpath, "r", encoding='utf-8') as fin:
     t = fin.read()
   if deriv_r.search(t):
-    return deriv_r.findall(t)[0]
+    return deriv_r.findall(t)[0], 1
   elif origin_r.search(t):
-    return origin_r.findall(t)[0]
+    return origin_r.findall(t)[0], 2
   elif result_r.search(t):
-    return "other"
+    return "other", 0
   else:
-    return "not a word"
+    return "not a word", -1
     
 def divide_deriv_origin_other_no(wordlist, oxford_dir="oxford",
                                  deriv_rule=r'<p class="derivative_of">See <a href="(.*?)">\1</a></p>',
-                                 origin_rule=r'<section class="etymology etym"><h3><strong>Origin</strong></h3><div class="senseInnerWrapper"><p>(.*?)</p></div></section>'):
+                                 origin_rule=r'<section class="etymology etym"><h3><strong>Origin</strong></h3><div class="senseInnerWrapper"><p>(.*?)</p></div></section>',
+                                 result_rule=r'<meta property="og:title" content="([^/<>]*?)" />'):
   
   d = {}
+  deriv_r = re.compile(deriv_rule)
+  origin_r = re.compile(origin_rule)
+  result_r = re.compile(result_rule)
   for word in wordlist:
-    d[word] = __divide_single_word(word, oxford_dir=oxford_dir, deriv_rule=deriv_rule, origin_rule=origin_rule)
-    
+    d[word], _ = __divide_single_word(word, oxford_dir=oxford_dir, deriv_r=deriv_r, origin_r=origin_r, result_r=result_r)
   return d
 
 
+def divide_deriv_origin_other_no_to_csv(wordlist, oxford_dir="oxford",
+                                 deriv_rule=r'<p class="derivative_of">See <a href="(.*?)">\1</a></p>',
+                                 origin_rule=r'<section class="etymology etym"><h3><strong>Origin</strong></h3><div class="senseInnerWrapper"><p>(.*?)</p></div></section>',
+                                 result_rule=r'<meta property="og:title" content="([^/<>]*?)" />'):
+  deriv_r = re.compile(deriv_rule)
+  origin_r = re.compile(origin_rule)
+  result_r = re.compile(result_rule)
+  data = []
+  cnt = 0
+  code_dict = {}
+  t0 = time.time()
+  for word in wordlist:
+    detail, code = __divide_single_word(word, oxford_dir=oxford_dir, deriv_r=deriv_r, origin_r=origin_r, result_r=result_r)
+    data.append([word, detail, code])
+    if code in code_dict:
+      code_dict[code] += 1
+    else:
+      code_dict[code] = 1
+    cnt += 1
+    if cnt % 100 == 0:
+      delta_t = time.time() - t0
+      print(cnt, code_dict, delta_t*(13200-cnt)/cnt/60)
+      t0 = time.time()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  df = pd.DataFrame(data, columns=['word', 'detail', 'code'])
+  df.to_csv("oxford_full_1209.csv", encoding="utf-8")
+  return df
 
 
 if __name__ == "__main__":
@@ -146,5 +155,6 @@ if __name__ == "__main__":
   '''
   with open("to_crawl_all_words.txt", "r", encoding='utf-8') as fin:
     wl = fin.read().split('\n')
-  d = divide_deriv_origin_other_no(wl)
-  print(d)
+  df = divide_deriv_origin_other_no_to_csv(wl)
+  print("done")
+  
